60fad50ee1f0c39114403dc170281f9ad55fb408
Alexander Fust <afust@gams.com>
2017-07-25 17:53:49 +0200

    Transport10 refactored.

--- examples/transport10/transport10.cpp
+++ examples/transport10/transport10.cpp
@@ -23,8 +23,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-
-#include "transport10.h"
 #include "gams.h"
 #include "gamssymbolrecord.h"
 
@@ -34,84 +32,7 @@
 using namespace gams;
 using namespace std;
 
-/// This is the 10th model in a series of tutorial examples. Here we show:
-///   - How to fill a GAMSDatabase by reading from MS Excel
-int Transport10::becomes_main(int argc, char* argv[])
-{
-    GAMSWorkspaceInfo wsInfo;
-    wsInfo.setDebug(GAMSEnum::DebugLevel::ShowLog);
-    if (argc > 1)
-        wsInfo.setSystemDirectory(argv[1]);
-    GAMSWorkspace ws(wsInfo);
-
-    // TODO(AF) add excel stuff
-    // Reading input data from workbook
-    //    var excelApp = new Excel.Application();
-    //    Excel.Workbook wb = excelApp.Workbooks.Open(Path.Combine(ws.SystemDirectory, @"apifiles/Data/transport.xls"));
-
-    //    Excel.Range range;
-
-    //    Excel.Worksheet capacity = (Excel.Worksheet)wb.Worksheets.get_Item("capacity");
-    //    range = capacity.UsedRange;
-    //    Array capacityData = (Array)range.Cells.Value;
-    //    int iCount = capacity.UsedRange.Columns.Count;
-
-    //    Excel.Worksheet demand = (Excel.Worksheet)wb.Worksheets.get_Item("demand");
-    //    range = demand.UsedRange;
-    //    Array demandData = (Array)range.Cells.Value;
-    //    int jCount = range.Columns.Count;
-
-    //    Excel.Worksheet distance = (Excel.Worksheet)wb.Worksheets.get_Item("distance");
-    //    range = distance.UsedRange;
-    //    Array distanceData = (Array)range.Cells.Value;
-
-    //    // number of markets/plants have to be the same in all spreadsheets
-    //    Debug.Assert((range.Columns.Count - 1) == jCount && (range.Rows.Count - 1) == iCount,
-    //                 "Size of the spreadsheets doesn't match");
-    //    wb.Close();
-
-    // Creating the GAMSDatabase and fill with the workbook data
-    GAMSDatabase db = ws.addDatabase();
-
-    GAMSSet i = db.addSet("i", 1, "Plants");
-    GAMSSet j = db.addSet("j", 1, "Markets");
-    GAMSParameter capacityParam = db.addParameter("a", "Capacity", i);
-    GAMSParameter demandParam = db.addParameter("b", "Demand", j);
-    vector<GAMSDomain> sets {i, j};
-    GAMSParameter distanceParam = db.addParameter("d", "Distance", sets);
-
-    cout << "Transport10: not implementet yet." << endl;
-
-    //    for (int ic = 1; ic <= iCount; ic++)
-    //    {
-    //        i.AddRecord((string)capacityData.GetValue(1, ic));
-    //        capacityParam.AddRecord((string)capacityData.GetValue(1, ic)).Value = (double)capacityData.GetValue(2, ic);
-    //    }
-    //    for (int jc = 1; jc <= jCount; jc++)
-    //    {
-    //        j.AddRecord((string)demandData.GetValue(1, jc));
-    //        demandParam.AddRecord((string)demandData.GetValue(1, jc)).Value = (double)demandData.GetValue(2, jc);
-    //        for (int ic = 1; ic <= iCount; ic++)
-    //        {
-    //            distanceParam.AddRecord((string)distanceData.GetValue(ic + 1, 1), (string)distanceData.GetValue(1, jc + 1)).Value = (double)distanceData.GetValue(ic + 1, jc + 1);
-    //        }
-    //    }
-
-    // Create and run the GAMSJob
-    //    GAMSOptions opt = ws.addOptions();
-    //    GAMSJob t10 = ws.addJobFromString(getModelText());
-    //    opt.mDefines["gdxincname"] = db.name();
-    //    opt.setAllModelTypes("xpress");
-    //    t10.run(opt, db);
-    //    for (GAMSVariableRecord record : t10.outDB().getVariable("x"))
-    //        cout << "x(" << record.key(0) << "," << record.key(1) << "): level=" << record.level() <<
-    //                " marginal=" << record.marginal() << endl;
-
-    return 0;
-}
-
-
-string Transport10::getModelText()
+string getModelText()
 {
     return  "  Sets                                                                     \n"
             "       i   canning plants                                                  \n"
@@ -154,4 +75,81 @@ string Transport10::getModelText()
             "  Solve transport using lp minimizing z ;                                  \n"
             "                                                                           \n"
             "  Display x.l, x.m ;                                                       \n";
+}
+
+/// This is the 10th model in a series of tutorial examples. Here we show:
+///   - How to fill a GAMSDatabase by reading from MS Excel
+int main(int argc, char* argv[])
+{
+    cout << "---------- Transport 10 --------------" << endl;
+    cout << "Transport10: not implementet yet." << endl;
+
+    GAMSWorkspaceInfo wsInfo;
+    wsInfo.setDebug(GAMSEnum::DebugLevel::ShowLog);
+    if (argc > 1)
+        wsInfo.setSystemDirectory(argv[1]);
+    GAMSWorkspace ws(wsInfo);
+
+    // TODO(AF) add excel stuff
+    // Reading input data from workbook
+    //    var excelApp = new Excel.Application();
+    //    Excel.Workbook wb = excelApp.Workbooks.Open(Path.Combine(ws.SystemDirectory, @"apifiles/Data/transport.xls"));
+
+    //    Excel.Range range;
+
+    //    Excel.Worksheet capacity = (Excel.Worksheet)wb.Worksheets.get_Item("capacity");
+    //    range = capacity.UsedRange;
+    //    Array capacityData = (Array)range.Cells.Value;
+    //    int iCount = capacity.UsedRange.Columns.Count;
+
+    //    Excel.Worksheet demand = (Excel.Worksheet)wb.Worksheets.get_Item("demand");
+    //    range = demand.UsedRange;
+    //    Array demandData = (Array)range.Cells.Value;
+    //    int jCount = range.Columns.Count;
+
+    //    Excel.Worksheet distance = (Excel.Worksheet)wb.Worksheets.get_Item("distance");
+    //    range = distance.UsedRange;
+    //    Array distanceData = (Array)range.Cells.Value;
+
+    //    // number of markets/plants have to be the same in all spreadsheets
+    //    Debug.Assert((range.Columns.Count - 1) == jCount && (range.Rows.Count - 1) == iCount,
+    //                 "Size of the spreadsheets doesn't match");
+    //    wb.Close();
+
+    // Creating the GAMSDatabase and fill with the workbook data
+    GAMSDatabase db = ws.addDatabase();
+
+    GAMSSet i = db.addSet("i", 1, "Plants");
+    GAMSSet j = db.addSet("j", 1, "Markets");
+    GAMSParameter capacityParam = db.addParameter("a", "Capacity", i);
+    GAMSParameter demandParam = db.addParameter("b", "Demand", j);
+    vector<GAMSDomain> sets {i, j};
+    GAMSParameter distanceParam = db.addParameter("d", "Distance", sets);
+
+    //    for (int ic = 1; ic <= iCount; ic++)
+    //    {
+    //        i.AddRecord((string)capacityData.GetValue(1, ic));
+    //        capacityParam.AddRecord((string)capacityData.GetValue(1, ic)).Value = (double)capacityData.GetValue(2, ic);
+    //    }
+    //    for (int jc = 1; jc <= jCount; jc++)
+    //    {
+    //        j.AddRecord((string)demandData.GetValue(1, jc));
+    //        demandParam.AddRecord((string)demandData.GetValue(1, jc)).Value = (double)demandData.GetValue(2, jc);
+    //        for (int ic = 1; ic <= iCount; ic++)
+    //        {
+    //            distanceParam.AddRecord((string)distanceData.GetValue(ic + 1, 1), (string)distanceData.GetValue(1, jc + 1)).Value = (double)distanceData.GetValue(ic + 1, jc + 1);
+    //        }
+    //    }
+
+    // Create and run the GAMSJob
+    //    GAMSOptions opt = ws.addOptions();
+    //    GAMSJob t10 = ws.addJobFromString(getModelText());
+    //    opt.mDefines["gdxincname"] = db.name();
+    //    opt.setAllModelTypes("xpress");
+    //    t10.run(opt, db);
+    //    for (GAMSVariableRecord record : t10.outDB().getVariable("x"))
+    //        cout << "x(" << record.key(0) << "," << record.key(1) << "): level=" << record.level() <<
+    //                " marginal=" << record.marginal() << endl;
+
+    return 0;
 }
