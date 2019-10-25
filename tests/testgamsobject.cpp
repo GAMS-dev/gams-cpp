f32256fb3676949d76ddb02bbe3755dcf8175fa0
mdekhil <dekhil.medali@gmail.com>
2019-10-25 15:59:36 +0200

    update license

--- tests/testgamsobject.cpp
+++ tests/testgamsobject.cpp
@@ -1,7 +1,7 @@
 /*
  * GAMS - General Algebraic Modeling System C++ API
  *
- * Copyright (c) 2017-2018 GAMS Software GmbH <support@gams.com>
+ * Copyright (c) 2017-2019 GAMS Software GmbH <support@gams.com>
  * Copyright (c) 2017 Jarungjit Parnjai <jeed@gams.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
