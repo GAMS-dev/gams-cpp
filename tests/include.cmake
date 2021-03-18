9a0703ab4193af2176b5b006cc018d8864102050
Robin "Rogo" Goltermann <rgoltermann@gams.com>
2021-03-18 15:24:22 +0100

    moved googletest, enabled gamsdatabase

--- tests/include.cmake
+++ tests/include.cmake
@@ -32,9 +32,10 @@ else()
     set(BASEPATH "${GAMSPATH}/apifiles/")
 endif()
 
-include_directories("${BASEPATH}/C/api"
+include_directories("${GAMSPATH}/apifiles/C/api"
                     "${BASEPATH}/C++/api")
 if(WIN32)
+    # TODO(RG): this is wrong. cmake base path does not exist
     set(VSVERSION "vs2019" CACHE STRING "Visual Studio version")
     link_directories("${BASEPATH}/C++/lib/${VSVERSION}")
 else()
@@ -58,20 +59,3 @@ if(UNIX) # Apple or Linux
         endif()
     endif()
 endif()
-
-#TODO(RG): move this to its own subproject
-# GoogleTest
-include_directories(${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_SOURCE_DIR}/../src)
-if(NOT DEFINED ${GOOGLETEST_DIR})
-    set(GOOGLETEST_DIR "${CMAKE_CURRENT_SOURCE_DIR}/../../googletest")
-endif()
-
-set(GTEST_SRCDIR ${GOOGLETEST_DIR}/googletest)
-include_directories(${GTEST_SRCDIR} ${GTEST_SRCDIR}/include)
-
-file(GLOB_RECURSE google_test_headers ${GTEST_SRCDIR}/include/*.h)
-add_library(gtest STATIC ${GTEST_SRCDIR}/src/gtest-all.cc ${GTEST_SRCDIR}/src/gtest_main.cc ${google_test_headers})
-
-enable_testing()
-
-
