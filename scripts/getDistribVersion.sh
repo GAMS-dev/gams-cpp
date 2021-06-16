0158eeb9361f46a5d51add014e0784bd5091c94c
mdekhil <dekhil.medali@gmail.com>
2021-06-16 12:24:06 +0200

    updated/added License

--- scripts/getDistribVersion.sh
+++ scripts/getDistribVersion.sh
@@ -1,9 +1,7 @@
 # GAMS - General Algebraic Modeling System C++ API
 #
-# GAMS - General Algebraic Modeling System C++ API
-#
-# Copyright (c) 2017 GAMS Software GmbH <support@gams.com>
-# Copyright (c) 2017 GAMS Development Corp. <support@gams.com>
+# Copyright (c) 2017-2021 GAMS Software GmbH <support@gams.com>
+# Copyright (c) 2017-2021 GAMS Development Corp. <support@gams.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
